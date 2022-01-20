3ad05e2a7d9f4b34ef072927c37b3a3c4d214acc
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2022-01-19 18:28:08 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Installation
+
+````
+~ composer install
+~ npm install && npm run prod
+~ php artisan serve
+````
---
+++ resources/views/components.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="fw-bold fs-1 text-gray-800">Blade Components</h2>
+    </x-slot>
+
+    <!-- Add Content Here -->
+
+</x-app-layout>
